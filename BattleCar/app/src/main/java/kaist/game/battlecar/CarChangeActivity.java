34075be9660984ef1e30d63b38d11b06046c899a
bullpae <bullpae@gmail.com>
2015-12-13 01:56:49 +0900

    [Change] CarChangeActivity
    - 추가한 WifiConnector Class를 이용하여 처리

--- BattleCar/app/src/main/java/kaist/game/battlecar/CarChangeActivity.java
+++ BattleCar/app/src/main/java/kaist/game/battlecar/CarChangeActivity.java
@@ -24,15 +24,14 @@ import java.util.List;
 
 import kaist.game.battlecar.adapter.CarApList;
 import kaist.game.battlecar.adapter.CarApListAdapter;
+import kaist.game.battlecar.util.WifiConnector;
 
 public class CarChangeActivity extends Activity implements AdapterView.OnItemClickListener {
     private final static String TAG = CarChangeActivity.class.getSimpleName();
-    private ScanResult scanResult;
-    private WifiManager wm;
-    private List apList;
     private ArrayList<CarApList> arrList = new ArrayList<CarApList>();
     private CarApListAdapter carListAdapter;
     private SharedPreferences setting;
+    private WifiConnector wifi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +39,7 @@ public class CarChangeActivity extends Activity implements AdapterView.OnItemCli
         setContentView(R.layout.activity_car_change);
 
         setting =  PreferenceManager.getDefaultSharedPreferences(this);
+        wifi = new WifiConnector(this, setting.getString("vittles_ap_prefix", ""));
 
         searchVittles();
     }
@@ -54,21 +54,7 @@ public class CarChangeActivity extends Activity implements AdapterView.OnItemCli
     };
 
     public void searchWifi() {
-        apList = wm.getScanResults();
-        if (wm.getScanResults() != null) {
-            arrList.clear(); // 이전 리스트 삭제
-            int size = apList.size();
-            for (int i = 0; i < size; i++) {
-                scanResult = (ScanResult) apList.get(i);
-                Log.d(TAG, "onActivityResult " + scanResult);
-
-                String apPrefix = setting.getString("vittles_ap_prefix", "");
-                if (scanResult.SSID.toUpperCase().contains(apPrefix)) {
-                    CarApList item = new CarApList(scanResult.SSID, scanResult.capabilities);
-                    arrList.add(item);
-                }
-            }
-        }
+        arrList = wifi.searchWifi();
 
         if (arrList.size() > 0) {
             carListAdapter = new CarApListAdapter(this, R.layout.list_car_ap, arrList);
@@ -77,10 +63,10 @@ public class CarChangeActivity extends Activity implements AdapterView.OnItemCli
             list.setOnItemClickListener(this);
         } else {
             // todo : VITTLES 없으면 메세지 표시(재검색 또는 중지)
+            Toast.makeText(this, "VITTLES가 검색 안되었습니다.", Toast.LENGTH_LONG).show();
         }
     }
 
-//    // ListView 안에 Item을 클릭시에 호출되는 Listener
     @Override
     public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
         String ssid = arrList.get(position).getCarApSSID();
@@ -93,48 +79,8 @@ public class CarChangeActivity extends Activity implements AdapterView.OnItemCli
         edit.putString("my_vittles_ap", ssid);
         edit.commit();
 
-        // wifi에 접속
-        // Password 물어보고 접속
-        WifiConfiguration wfc = new WifiConfiguration();
-        wfc.SSID = "\"".concat(ssid).concat("\"");
-        wfc.status = WifiConfiguration.Status.DISABLED;
-        wfc.priority = 40;
-
-        if (capa.toUpperCase().contains("Open")) {
-            wfc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
-            wfc.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
-            wfc.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
-            wfc.allowedAuthAlgorithms.clear();
-            wfc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
-
-            wfc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
-            wfc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
-            wfc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
-            wfc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
-        } else if (capa.toUpperCase().contains("WEP")) {
-            String password = "intintint";
-            wfc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
-            wfc.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
-            wfc.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
-            wfc.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
-            wfc.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.SHARED);
-            wfc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
-
-            wfc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
-            wfc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
-            wfc.wepKeys[0] = "\"".concat(password).concat("\"");
-            wfc.wepTxKeyIndex = 0;
-        } else if (capa.toUpperCase().contains("WPA")) {
-            String password = "intintint";
-            wfc.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
-            wfc.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
-            wfc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
-            wfc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
-            wfc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
-            wfc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
-            wfc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
-            wfc.preSharedKey = "\"".concat(password).concat("\"");
-        } else {
+        int ret = wifi.Connecting(ssid, capa, "intintint");
+        if (ret == -1) {
             AlertDialog.Builder alert = new AlertDialog.Builder(this);
             alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
                 @Override
@@ -145,12 +91,10 @@ public class CarChangeActivity extends Activity implements AdapterView.OnItemCli
             alert.setMessage("Capabilites Error!!");
             alert.show();
         }
-
-    };
+    }
 
     public void searchVittles() {
-        wm = (WifiManager) getSystemService(WIFI_SERVICE); // WifiManager 초기화
-        wm.startScan(); // 검색
+//        wifi.init();
 
         IntentFilter filter = new IntentFilter();
         filter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
@@ -159,11 +103,11 @@ public class CarChangeActivity extends Activity implements AdapterView.OnItemCli
 
     public void onSearchBtnClicked(View v) {
         searchVittles();
+        Toast.makeText(this, "Searching...", Toast.LENGTH_LONG).show();
     }
 
     public void onHomeBtnClicked(View v) {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent);
     }
-
 }
